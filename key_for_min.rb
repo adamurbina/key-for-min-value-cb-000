d09c2891e9dc1298b267ea0bd15dbf4d93df00cc
Adam Urbina <adamurbina@gmail.com>
2018-06-01 19:00:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,12 @@ require 'pry'
 def key_for_min_value(name_hash)
     key, value = name_hash.first
     lowest = [key, value]
-    binding.pry
+    #binding.pry
     key_for_min_value.each { |key, value|
         if value < lowest[1]
             lowest[0] = key
             lowest[1] = value
+            puts lowest
         end}
     lowest[0]
 end
